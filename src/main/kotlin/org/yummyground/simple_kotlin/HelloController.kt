52f3f8cf53efd2709a2d2411a8ab8e14afe2675b
yummygyudon <bang2brew@gmail.com>
2025-03-04 22:17:07 +0900

    feat : practice for using Logger

--- src/main/kotlin/org/yummyground/simple_kotlin/HelloController.kt
+++ src/main/kotlin/org/yummyground/simple_kotlin/HelloController.kt
@@ -1,13 +1,23 @@
 package org.yummyground.simple_kotlin
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.reflect.jvm.jvmName
 
 @RestController
-class HelloController {
+class HelloController(
+    val loggerForSimpleName: Logger = LoggerFactory.getLogger(HelloController::class.simpleName),
+    val loggerForQualifiedName: Logger = LoggerFactory.getLogger(HelloController::class.qualifiedName),
+    val loggerForJvmName: Logger = LoggerFactory.getLogger(HelloController::class.jvmName)
+) {
 
     @GetMapping
     fun hello(): String {
+        loggerForSimpleName.error("Logging Test on Spring Application")
+        loggerForQualifiedName.error("Logging Test on Spring Application")
+        loggerForJvmName.error("Logging Test on Spring Application")
         return "Hello Controller - common method"
     }
 
